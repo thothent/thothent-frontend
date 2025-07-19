f165b0d095821dce36a566148e2b0b41f25852a4
jonas <jonaspfeifer@drei.at>
2025-07-19 22:56:48 +0200

    added basic page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import type { Component } from 'solid-js';
+import style from "./App.module.css"
 
 const App: Component = () => {
   return (
-    <div>
-	<h1>Hello World</h1>
+    <div id="app">
+	<div id={style.container}>
+		<div id={style.heading}>
+			<img id={style.logo} src="http://localhost:8080/assets/logo.svg" />
+			<div id={style.title}>Thothent</div>
+		</div>
+		<div id={style.input_container}>
+			<img id={style.search_icon} src="http://localhost:8080/assets/search.svg" />
+			<input id={style.input} placeholder="Enter your search..."/>
+		</div>
+	</div>
     </div>
   );
 };
